c991cd743f491749abf2a9603e75bc9bbbfebc39
MinkyeongSung~ <wildlegion59@gmail.com>
2024-01-09 21:38:14 +0900

    오류 체크중

--- lib/main.dart
+++ lib/main.dart
@@ -22,23 +22,23 @@ GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 //   // 권한 요청 및 결과 처리
-//   bool hasPermissions = await PermissionManager.requestPermissions();
-//
-//   if (hasPermissions) {
-//     await Firebase.initializeApp(
-//       options: DefaultFirebaseOptions.currentPlatform,
-//     );
-//     runApp(ProviderScope(child: const MyApp()));
-//   } else {
-//     // 권한이 거부된 경우 처리
-//     // 예를 들어 사용자에게 권한이 필요하다는 다이얼로그를 표시하거나 다른 조치를 취할 수 있습니다.
-//   }
-// }
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(ProviderScope(child: const MyApp()));
+  bool hasPermissions = await PermissionManager.requestPermissions();
+
+  if (hasPermissions) {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    runApp(ProviderScope(child: const MyApp()));
+  } else {
+    // 권한이 거부된 경우 처리
+    // 예를 들어 사용자에게 권한이 필요하다는 다이얼로그를 표시하거나 다른 조치를 취할 수 있습니다.
+  }
 }
+//   await Firebase.initializeApp(
+//     options: DefaultFirebaseOptions.currentPlatform,
+//   );
+//   runApp(ProviderScope(child: const MyApp()));
+// }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
