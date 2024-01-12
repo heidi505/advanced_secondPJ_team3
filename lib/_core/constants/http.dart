e983502639534c63f5539cffc68bd92259750442
heidi505 <xxwhite19@gmail.com>
2024-01-12 16:46:30 +0900

    베이스 url 널 인 현상

--- lib/_core/constants/http.dart
+++ lib/_core/constants/http.dart
@@ -4,7 +4,7 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 //은지
 //String baseUrl = "http://192.168.0.165:8080";
 //하얀
-//String baseUrl = "http://192.168.0.201:8080";
+String baseUrl = "http://192.168.0.201:8080";
 //누군가의 baseUrl
 // String baseUrl = "http://192.168.45.50:8080";
 // 박남규 - baseUrl: "http://192.168.0.71:8080"
@@ -21,7 +21,7 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 // http 통신
 final dio = Dio(
   BaseOptions(
-      baseUrl: "http://192.168.0.71:8080", // 내 IP 입력
+      baseUrl: baseUrl, // 내 IP 입력
       contentType: "application/json; charset=utf-8"),
 );
 
