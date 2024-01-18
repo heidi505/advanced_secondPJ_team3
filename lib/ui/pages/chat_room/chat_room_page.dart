84d15bb97665bec8f9b64772171d2174b42c781d
heidi505 <xxwhite19@gmail.com>
2024-01-18 21:06:17 +0900

    일단 커밋

--- lib/ui/pages/chat_room/chat_room_page.dart
+++ lib/ui/pages/chat_room/chat_room_page.dart
@@ -38,7 +38,7 @@ class _ChatRoomPageState extends ConsumerState<ChatRoomPage> {
     SessionUser session = ref.read(sessionProvider);
 
     if(isFirst) {
-      ref.read(otherChatProvider.notifier).notifyInit();
+      ref.read(otherChatProvider.notifier).fetchMessages();
       isFirst = false;
     }
 
